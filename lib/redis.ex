a64dbea7a6e363174870d0e34180a039dace5a24
debora-be <arobedbee@gmail.com>
2022-08-25 14:51:03 -0300

    initial commit

---
+++ lib/redis.ex
@@ -0,0 +1,18 @@
+defmodule Redis do
+  @moduledoc """
+  Documentation for `Redis`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Redis.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
